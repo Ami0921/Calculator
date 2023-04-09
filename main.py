289ceb1536ce3766cc6a121735e8cb55217038eb
Ami0921 <92774056+Ami0921@users.noreply.github.com>
2023-04-08 22:10:04 -0400

    Get value and operation from user completed. (#2)
    * Get value and operation from user completed.
    
    * addressed PR comments.

--- main.py
+++ main.py
@@ -13,6 +13,27 @@ class Calc:
               "For Multiplication type Multi\n"
               "For Division type Div\n"
               "For Break the Calculation type N")
+        self.var1 = self.get_number()
+
+        # get values until user press n or no
+        while True:
+            self.get_operation()
+            if self.opt.lower() in ['n', 'no']:
+                break
+            self.var2 = self.get_number()
+
+    def get_number(self):
+        number = input("Enter Value : ")
+        self.history.append(number)
+        try:
+            number = float(number)
+        except ValueError:
+            raise ValueError(f'Please enter number instead ({number})')
+        return number
+
+    def get_operation(self):
+        self.opt = input("Enter Operation : ")
+        self.history.append(self.opt)
 
 calc = Calc()
 calc.main()
